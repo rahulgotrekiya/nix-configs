7da47217744672388515d41c64b5743807b08c27
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-11-25 23:24:38 +0530

    change serif fonts from Inter to Libre Baskerville

--- home/themes/theme.nix
+++ home/themes/theme.nix
@@ -1,40 +1,16 @@
 { libs, config, pkgs, ... }:
 
 { 
-  gtk = {
-    enable = true;
-    font.name = "Inter";
-    font.size = 11;
-    theme = {
-      package = pkgs.adw-gtk3;
-      name = "adw-gtk3-dark";
-    };
-  };
 
-  qt = {
-    enable = true;
-    platformTheme.name = "gtk3";
-    style = {
-      name = "adwaita-dark";
-      package = pkgs.adwaita-qt;
-    };
-  };
 
   fonts = {
     fontconfig = { 
       enable = true;
       defaultFonts = {
-        serif = [ "Inter" ];
+        serif = [ "Libre Baskerville" ];
         sansSerif = [ "Inter" ];
         monospace = [ "JetBrainsMono Nerd Font" ];
       };
     };
-  };
-
-  # Add environment variables to enforce dark theme
-  home.sessionVariables = {
-    GTK_THEME = "Adwaita:dark";
-    # Force Electron/Chromium apps to use dark mode
-    FORCE_DARK_MODE = "1";
   };
 }
