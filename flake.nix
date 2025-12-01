c8fcc98ee909a69549130df5cb99cc235e263ca7
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-12-01 23:52:59 +0530

    add homelab configurations

--- flake.nix
+++ flake.nix
@@ -7,24 +7,44 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-   outputs = { self, nixpkgs, home-manager, ... }:
+  outputs = { self, nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       lib = nixpkgs.lib;
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
+        # Laptop/desktop configuration
         nixos = lib.nixosSystem {
           inherit system;
           modules = [ ./nixos/configuration.nix ];
         };
+
+        # home server configuration
+        homelab = lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            meta = { 
+              hostname = "homelab";
+            };
+          };
+          modules = [
+            ./homelab/configuration.nix
+          ];
+        };
       };
+
       homeConfigurations = {
         rahul = home-manager.lib.homeManagerConfiguration {
           inherit pkgs;
           modules = [ ./home/home.nix ];
         };
+        
+        # Optional: separate home-manager config for homelab user
+        # neo = home-manager.lib.homeManagerConfiguration {
+        #   inherit pkgs;
+        #   modules = [ ./homelab/home.nix ];
+        # };
       };
     };
 }
-
