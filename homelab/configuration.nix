4c8a318d3d40210c994096a0f66a356226accff2
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2025-12-02 15:44:15 +0530

    setup firewall on homelab

--- homelab/configuration.nix
+++ homelab/configuration.nix
@@ -104,27 +104,23 @@
     };
   };
  
-  # Open ports in the firewall
-  # networking.firewall.allowedTCPPorts = [ 80 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether
-  networking.firewall.enable = false;
+  # Firewall
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [ 
+      22    # SSH
+      80    # HTTP
+      443   # HTTPS
+      8096  # Jellyfin
+      8989  # Sonarr
+      7878  # Radarr
+      9696  # Prowlarr
+      8686  # Lidarr
+      6767  # Bazarr
+    ];
+    allowedUDPPorts = [ 
+    ];
+  };
 
-  # This option defines the first version of NixOS you have installed on this particular machine,
-  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
-  #
-  # Most users should NEVER change this value after the initial install, for any reason,
-  # even if you've upgraded your system to a new NixOS release.
-  #
-  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
-  # so changing it will NOT upgrade your system.
-  #
-  # This value being lower than the current NixOS release does NOT mean your system is
-  # out of date, out of support, or vulnerable.
-  #
-  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
-  # and migrated your data accordingly.
-  #
-  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
-  system.stateVersion = "23.11"; # Did you read the comment?
+ system.stateVersion = "23.11"; # Did you read the comment?
 }
