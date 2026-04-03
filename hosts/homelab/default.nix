abd533d7b0d6bb621ff75df687e780ef6f4d1df1
rahul gotrekiya <121397381+RahulGotrekiya@users.noreply.github.com>
2026-04-03 13:51:16 +0530

    fix: add acme token

--- hosts/homelab/default.nix
+++ hosts/homelab/default.nix
@@ -23,12 +23,13 @@
     HandleLidSwitchExternalPower = "ignore";
     HandleLidSwitchDocked = "ignore";
   };
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
+
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = "no";
+    AllowHibernation = "no";
+    AllowHybridSleep = "no";
+    AllowSuspendThenHibernate = "no";
+  };
 
   # Tailscale as exit node / subnet router
   services.tailscale.useRoutingFeatures = "server";
